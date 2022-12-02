36ebfa242a4acc56a740bafde9da46e0ea80162f
Xianlong Zhang <k20083732@kcl.ac.uk>
2022-12-02 11:47:14 +0000

    done with form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,22 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+from .models import User
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name']
+
+
+        label = 'Quantity'
+        widgets = { 'quantity':forms.NumberInput() }
+        validators=[RegexValidator(
+            regex=r'^(?=.*[0-9]).*$',
+            message='Quantity must be a number.'
+
+        label2 = 'Description'
+        widgets = { 'Description':forms.Textarea() }
+     )]
